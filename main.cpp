a1842c262621884bbec432cd51cea5b328cdfbc3
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2015-03-23 08:38:54 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "store.h"
+
+using namespace std;
+
+int main()
+{
+	db_store Shop(77, "Item 4", 0, 99, 2, 4);
+	cout << "Count: " << Shop.getCount() << endl;
+	cout << "Item id: " << Shop.getItem_id() << endl;
+	cout << "Item name: " << Shop.getItem_name() << endl;
+	cout << "Item price: " << Shop.getProice() << endl;
+	cout << "Item quanity: " << Shop.getQuantity() << endl;
+	cout << "Store name: " << Shop.getStore_num() << endl;
+
+	system("pause");
+
+	return 0;
+}
